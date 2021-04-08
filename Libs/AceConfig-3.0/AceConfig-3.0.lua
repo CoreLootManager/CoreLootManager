3499562aacb3d2895f25e0647d89ec8770ee85b3
lantisnt <lantisnt@gmail.com>
2021-04-08 11:02:27 +0200

    Update ACE3

--- Libs/AceConfig-3.0/AceConfig-3.0.lua
+++ Libs/AceConfig-3.0/AceConfig-3.0.lua
@@ -12,10 +12,10 @@ Very light wrapper library that combines all the AceConfig subcomponents into on
 
 ]]
 
-local cfgreg = LibStub("AceConfigRegistry-3.0-CLM")
-local cfgcmd = LibStub("AceConfigCmd-3.0-CLM")
+local cfgreg = LibStub("AceConfigRegistry-3.0")
+local cfgcmd = LibStub("AceConfigCmd-3.0")
 
-local MAJOR, MINOR = "AceConfig-3.0-CLM", 3
+local MAJOR, MINOR = "AceConfig-3.0", 3
 local AceConfig = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not AceConfig then return end
