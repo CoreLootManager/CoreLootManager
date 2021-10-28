cd10cbfe0f52cc7e8b86ced64a174a021d50014b
lantisnt <lantisnt@gmail.com>
2021-10-28 11:39:23 +0200

    Restore ACL

--- Modules/ACL/ACL.lua
+++ Modules/ACL/ACL.lua
@@ -23,34 +23,33 @@ end
 
 function ACL:CheckLevel(level, name)
     LOG:Trace("ACL:CheckLevel()")
-    return false
-    -- local info = GuildInfoListener:GetInfo()
-    -- -- By default block everything except for GM if level not provided
-    -- level = level or CONSTANTS.ACL.LEVEL.GUILD_MASTER
-    -- -- Request is for self
-    -- if name == nil then
-    --     if self.guildMaster then
-    --         return true
-    --     end
-    --     name = whoami()
-    -- end
-    -- local isGuildMaster = (info.guildMaster == name) or false
-    -- local isManager = info.managers[name] or false
-    -- local isAssistant = info.assistants[name] or false
-    -- -- Check for Guild Master
-    -- if level >= CONSTANTS.ACL.LEVEL.GUILD_MASTER then
-    --     return isGuildMaster
-    -- end
-    -- -- Check for Officer
-    -- if level >= CONSTANTS.ACL.LEVEL.MANAGER then
-    --     return isManager or isGuildMaster
-    -- end
-    -- -- Check for Managers
-    -- if level >= CONSTANTS.ACL.LEVEL.ASSISTANT then
-    --     return isAssistant or isManager or isGuildMaster
-    -- end
-    -- -- Check for unauthorized
-    -- return true
+    local info = GuildInfoListener:GetInfo()
+    -- By default block everything except for GM if level not provided
+    level = level or CONSTANTS.ACL.LEVEL.GUILD_MASTER
+    -- Request is for self
+    if name == nil then
+        if self.guildMaster then
+            return true
+        end
+        name = whoami()
+    end
+    local isGuildMaster = (info.guildMaster == name) or false
+    local isManager = info.managers[name] or false
+    local isAssistant = info.assistants[name] or false
+    -- Check for Guild Master
+    if level >= CONSTANTS.ACL.LEVEL.GUILD_MASTER then
+        return isGuildMaster
+    end
+    -- Check for Officer
+    if level >= CONSTANTS.ACL.LEVEL.MANAGER then
+        return isManager or isGuildMaster
+    end
+    -- Check for Managers
+    if level >= CONSTANTS.ACL.LEVEL.ASSISTANT then
+        return isAssistant or isManager or isGuildMaster
+    end
+    -- Check for unauthorized
+    return true
 end
 
 CONSTANTS.ACL = {}
