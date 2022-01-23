302b44de595fecdff42fa8d2e520ff61e85db728
lantisnt <lantisnt@gmail.com>
2022-01-23 09:03:24 +0100

    Fix tooltip error during addon ledger load

--- Modules/AuctionManager/GUI.lua
+++ Modules/AuctionManager/GUI.lua
@@ -433,7 +433,7 @@ function AuctionManagerGUI:GenerateAuctionOptions()
         bid_stats_info = {
             name = "Info",
             desc = (function()
-                if not RaidManager:IsInActiveRaid() or not self.raid then return "Not in raid" end
+                if not RaidManager:IsInActiveRaid() or self.raid == nil then return "Not in raid" end
                 -- Unique did any action dict
                 local didAnyAction = {}
                 -- generateInfo closure
