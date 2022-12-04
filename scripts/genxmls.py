a8157330c9f49afb65a5d7914052be0f32d46a75
lantisnt <lantisnt@gmail.com>
2022-12-04 18:03:53 +0100

    genxmls

--- scripts/genxmls.py
+++ scripts/genxmls.py
@@ -1,4 +1,5 @@
 from pathlib import PosixPath
+from io import TextIOWrapper
 
 FILELIST = '_filelist.xml'
 IGNORE_PREFIX = ["_", "."]
@@ -9,16 +10,37 @@ def beginsWithAny(i: str, b:list) -> bool:
             return True
     return False
 
-def writeFilelist(path: PosixPath):
+def isIgnored(p:PosixPath):
+    return beginsWithAny(p.as_posix(), IGNORE_PREFIX)
+
+def writeFilelistFile(fhandler: TextIOWrapper, files: list, dirs: list):
+    lines = []
+    lines.append('<Ui xmlns="http://www.blizzard.com/wow/ui/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.blizzard.com/wow/ui/')
+    lines.append('\t..\\FrameXML\\UI.xsd">')
+    for dir in dirs:
+        lines.append('<Include file="' + dir + '"/>')
+    for file in files:
+        lines.append('<Script file="' + file + '"/>')
+    lines.append('</Ui>')
+    fhandler.writelines(lines)
+
+def writeFilelist(path: PosixPath, base:PosixPath):
     if not path.is_dir(): return
-    
+    files = []
+    dirs = []
+    for node in path.iterdir():
+        node_relative = node.relative_to(base)
+        if node.is_dir():
+            dirs.append(node_relative.as_posix())
+            writeFilelist(node, path)
+        elif not isIgnored(node_relative):
+            files.append(node_relative.as_posix())
+    with open(path / FILELIST, "w", encoding="utf-8") as fhandler:
+        writeFilelistFile(fhandler, files, dirs)
 
+writeFilelist(PosixPath("."), PosixPath("."))
 
-BASE = PosixPath(".")
-
-
-
-for x in BASE.iterdir():
-    if x.is_dir():
-        for y in x.iterdir():
-            print(y, y.relative_to(x))
+# for x in BASE.iterdir():
+#     if x.is_dir():
+#         for y in x.iterdir():
+#             print(y, y.relative_to(x))
