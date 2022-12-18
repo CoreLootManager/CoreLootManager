7c3cf8180cf25cf63e28dc1f91118341ea6ef8a8
lantisnt <lantisnt@gmail.com>
2022-12-18 16:59:29 +0100

    Linking fixes

--- Modules/ProfileManager/ProfileManager.lua
+++ Modules/ProfileManager/ProfileManager.lua
@@ -136,6 +136,14 @@ function ProfileManager:Initialize()
                 if UTILS.typeof(self:GetProfileByGUID(mainProfile:Main()), CLM.MODELS.Profile) then return end
                 -- Do not allow alt chaining if alt has alts
                 if altProfile:HasAlts() then return end
+                -- Unlink old main
+                local currentMainProfile = self:GetProfileByGUID(altProfile:Main())
+                if UTILS.typeof(currentMainProfile, CLM.MODELS.Profile) then
+                    -- Remove main from this alt
+                    altProfile:ClearMain()
+                    -- Remove alt count from main
+                    currentMainProfile:RemoveAlt(altGUID)
+                end
                 -- Set new main of this alt
                 altProfile:SetMain(mainGUID)
                 -- Add alt to our main
@@ -285,7 +293,7 @@ function ProfileManager:MarkAsAltByNames(alt, main)
     LOG:Trace("ProfileManager:MarkAsAltByNames()")
     local altProfile = self:GetProfileByName(alt)
     if not UTILS.typeof(altProfile, CLM.MODELS.Profile) then
-        LOG:Error("MarkAsAltByNames(): Invalid alt")
+        LOG:Error("Alt does not exist. Aborting")
         return
     end
     local mainProfile = self:GetProfileByName(main)
@@ -293,17 +301,33 @@ function ProfileManager:MarkAsAltByNames(alt, main)
     if not UTILS.typeof(mainProfile, CLM.MODELS.Profile) then
         if altProfile:Main() ~= "" then
             CLM.MODULES.LedgerManager:Submit(CLM.MODELS.LEDGER.PROFILE.Link:new(altProfile:GUID(), nil), true)
+            LOG:Message(sformat("%s: %s", CLM.L["Unlink Alt"], altProfile:Name()))
+        else
+            LOG:Error("Main does not exist.")
         end
     else -- Link
         -- Do not allow alt chaining if main is alt
-        if UTILS.typeof(self:GetProfileByGUID(mainProfile:Main()), CLM.MODELS.Profile) then return end
+        if UTILS.typeof(self:GetProfileByGUID(mainProfile:Main()), CLM.MODELS.Profile) then 
+            LOG:Error("Alt chaining: Main is an alt.")
+            return
+        end
         -- Do not allow alt chaining if alt has alts
-        if altProfile:HasAlts() then return end
+        if altProfile:HasAlts() then
+            LOG:Error("Alt chaining: Alt has alts.")
+            return
+        end
         -- Don't allow self setting
-        if altProfile:GUID() == mainProfile:GUID() then return end
+        if altProfile:GUID() == mainProfile:GUID() then
+            LOG:Error("Alt and main are same character.")
+            return
+        end
         -- Don't allow re-setting
-        if altProfile:Main() == mainProfile:GUID() then return end
+        if altProfile:Main() == mainProfile:GUID() then
+            LOG:Error("Alt already set.")
+            return
+        end
         CLM.MODULES.LedgerManager:Submit(CLM.MODELS.LEDGER.PROFILE.Link:new(altProfile:GUID(), mainProfile:GUID()), true)
+        LOG:Message(sformat("%s%s %s", altProfile:Name(), CLM.L[" alt of:"], mainProfile:Name()))
     end
 end
 
