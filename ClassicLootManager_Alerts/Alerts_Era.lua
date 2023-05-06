d0c19a23ab5487f7ee2df8ae0e2d998e4039847a
lantisnt <lantisnt@gmail.com>
2023-05-06 07:59:26 +0200

    Alerts

--- ClassicLootManager_Alerts/Alerts_Era.lua
+++ ClassicLootManager_Alerts/Alerts_Era.lua
@@ -3,10 +3,10 @@
 -- Shamelessly taken from Gargul 
 -- https://github.com/papa-smurf/Gargul/blob/master/Classes/Alerts.lua
 CLM_ContainedAlertFrameMixin = {}
--- CLM_ContainedAlertFrameMixin.OnPostShow = ContainedAlertFrameMixin.OnPostShow
--- CLM_ContainedAlertFrameMixin.OnPostHide = ContainedAlertFrameMixin.OnPostHide
--- CLM_ContainedAlertFrameMixin.SetAlertContainer = ContainedAlertFrameMixin.SetAlertContainer
--- CLM_ContainedAlertFrameMixin.GetAlertContainer = ContainedAlertFrameMixin.GetAlertContainer
--- CLM_ContainedAlertFrameMixin.OnManagedAlertFrameVisibilityChanged = ContainedAlertFrameMixin.OnManagedAlertFrameVisibilityChanged
--- CLM_ContainedAlertFrameMixin.ManagesOwnOutroAnimation = ContainedAlertFrameMixin.ManagesOwnOutroAnimation
--- CLM_ContainedAlertFrameMixin.SetExternallyManagedOutroAnimation = ContainedAlertFrameMixin.SetExternallyManagedOutroAnimation
+CLM_ContainedAlertFrameMixin.OnPostShow = ContainedAlertFrameMixin.OnPostShow
+CLM_ContainedAlertFrameMixin.OnPostHide = ContainedAlertFrameMixin.OnPostHide
+CLM_ContainedAlertFrameMixin.SetAlertContainer = ContainedAlertFrameMixin.SetAlertContainer
+CLM_ContainedAlertFrameMixin.GetAlertContainer = ContainedAlertFrameMixin.GetAlertContainer
+CLM_ContainedAlertFrameMixin.OnManagedAlertFrameVisibilityChanged = ContainedAlertFrameMixin.OnManagedAlertFrameVisibilityChanged
+CLM_ContainedAlertFrameMixin.ManagesOwnOutroAnimation = ContainedAlertFrameMixin.ManagesOwnOutroAnimation
+CLM_ContainedAlertFrameMixin.SetExternallyManagedOutroAnimation = ContainedAlertFrameMixin.SetExternallyManagedOutroAnimation
