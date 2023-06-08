b7bd8381786100110509fe9dbf551dad0aa7dca0
lantisnt <lantisnt@gmail.com>
2023-06-08 11:19:45 +0200

    Initial tracker code

---
+++ ClassicLootManager_Tracker/Auction.lua
@@ -0,0 +1,30 @@
+-- ------------------------------- --
+local _, PRIV = ...
+local CLM = LibStub("ClassicLootManager").CLM
+-- ------ CLM common cache ------- --
+local LOG       = CLM.LOG
+local UTILS     = CLM.UTILS
+-- ------------------------------- --
+
+local dataColumn = {
+    name = CLM.L["Count"],
+    width = 36
+}
+
+local function dataCallback(auction, item, name, response)
+    local count = PRIV.MODULES.Tracker:GetCount(auction:GetRoster(), item:GetItemID(), name)
+    return {value = count or 0}
+end
+
+local function RegisterColumn()
+    CLM.GUI.AuctioManager:RegisterExternalColumn(dataColumn, dataCallback)
+end
+
+local Auction = {}
+function Auction:Initialize()
+    if PRIV.MODULES.Core:GetEnableAuctionColumn() then
+        RegisterColumn()
+    end
+end
+
+PRIV.MODULES.Auction = Auction
---
+++ ClassicLootManager_Tracker/Tracker.lua
@@ -0,0 +1,65 @@
+-- ------------------------------- --
+local _, PRIV = ...
+local CLM = LibStub("ClassicLootManager").CLM
+-- ------ CLM common cache ------- --
+local LOG       = CLM.LOG
+local UTILS     = CLM.UTILS
+-- ------------------------------- --
+
+
+local track = {}
+
+local function BuildItemTrack(roster, player, itemId)
+    local UID = roster:UID()
+    if not track[UID] then
+        track[UID] = {}
+    end
+    if not track[UID][player] then
+        track[UID][player] = {}
+    end
+    if not track[UID][player][itemId] then
+        track[UID][player][itemId] = {
+            count = 0,
+            list = {}
+        }
+    end
+
+    local profile = CLM.MODULES.ProfileManager:GetProfileByName(player)
+    if profile then
+        for _, loot in ipairs(roster:GetProfileLootByGUID(profile:GUID())) do
+            if loot:Id() == itemId then
+                track[UID][player][itemId].count = track[UID][player][itemId].count + 1
+                track[UID][player][itemId].list[#track[UID][player][itemId].list+1] = loot:Timestamp()
+            end
+        end
+    end
+end
+
+local function GetCount(roster, player, itemId)
+    return track[roster:UID()][player][itemId].count
+end
+
+local Tracker = {}
+function Tracker:Initialize()
+    self.track = track
+end
+
+function Tracker:GetCount(roster, player, itemId)
+    local success, count = pcall(GetCount, roster, player, itemId)
+    if not success then
+        BuildItemTrack(roster, player, itemId)
+        count = GetCount(roster, player, itemId)
+    end
+
+    return count
+end
+
+local function invalidateTrack(roster, loot, profile)
+    pcall(function()
+        track[roster:UID()][profile:Name()][loot:Id()] = nil
+    end)
+end
+hooksecurefunc(CLM.MODULES.RosterManager, "AddLootToRoster", invalidateTrack)
+
+
+PRIV.MODULES.Tracker = Tracker
