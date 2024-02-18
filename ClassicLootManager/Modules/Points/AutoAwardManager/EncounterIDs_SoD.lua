aa870ee0d725de07e2e645d0ae857ee42c30d162
Corey Durthaler <cdurth@gmail.com>
2024-02-17 21:55:51 -0500

    sod gnomeregan

--- ClassicLootManager/Modules/Points/AutoAwardManager/EncounterIDs_SoD.lua
+++ ClassicLootManager/Modules/Points/AutoAwardManager/EncounterIDs_SoD.lua
@@ -9,6 +9,13 @@ CLM.EncounterIDsMap[2704] = CLM.L["Gelihast"]
 CLM.EncounterIDsMap[2710] = CLM.L["Lorgus Jett"]
 CLM.EncounterIDsMap[2825] = CLM.L["Twilight Lord Kelris"]
 CLM.EncounterIDsMap[2891] = CLM.L["Aku'mai"]
+-- Gnomeregan
+CLM.EncounterIDsMap[217280] = CLM.L["Grubbis"]
+CLM.EncounterIDsMap[220007] = CLM.L["Viscous Fallout"]
+CLM.EncounterIDsMap[220072] = CLM.L["Electrocutioner 6000"]
+CLM.EncounterIDsMap[215728] = CLM.L["Crowd Pummeler 9-60"]
+CLM.EncounterIDsMap[218242] = CLM.L["The Mechanical Menagerie"]
+CLM.EncounterIDsMap[218537] = CLM.L["Mekgineer Thermaplugg"]
 
 
 CLM.EncounterIDs.SoD = {
@@ -23,6 +30,18 @@ CLM.EncounterIDs.SoD = {
             { id = 2710, name = CLM.EncounterIDsMap[2710] },
             { id = 2825, name = CLM.EncounterIDsMap[2825] },
             { id = 2891, name = CLM.EncounterIDsMap[2891] },
+        }
+    },
+	    {
+        name = CLM.L["Gnomeregan"],
+        difficulty = {3},
+        data = {
+            { id = 217280, name = CLM.EncounterIDsMap[217280] },
+            { id = 220007, name = CLM.EncounterIDsMap[220007] },
+            { id = 220072, name = CLM.EncounterIDsMap[220072] },
+            { id = 215728, name = CLM.EncounterIDsMap[215728] },
+            { id = 218242, name = CLM.EncounterIDsMap[218242] },
+            { id = 218537, name = CLM.EncounterIDsMap[218537] },
         }
     },
 }
