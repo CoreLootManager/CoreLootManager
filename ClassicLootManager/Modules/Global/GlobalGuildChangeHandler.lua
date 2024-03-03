d89a830c4eb9f310c4782b0413d4aad3eb3a79d5
lantisnt <lantisnt@gmail.com>
2024-03-03 11:18:04 +0100

    Handle guild change per player properly

--- ClassicLootManager/Modules/Global/GlobalGuildChangeHandler.lua
+++ ClassicLootManager/Modules/Global/GlobalGuildChangeHandler.lua
@@ -6,15 +6,20 @@ local LOG       = CLM.LOG
 -- local UTILS     = CLM.UTILS
 -- ------------------------------- --
 
-local GlobalGuildChangeHandler = {}
+local GlobalGuildChangeHandler = {
+    changeDetected = false
+}
 function GlobalGuildChangeHandler:Initialize()
-    CLM.MODULES.EventManager:RegisterWoWBucketEvent({"PLAYER_GUILD_UPDATE"}, 1, function()
+    CLM.MODULES.EventManager:RegisterWoWEvent({"PLAYER_GUILD_UPDATE"}, function(_, _, unitTarget)
+        if unitTarget ~= "player" then return end
+        if GlobalGuildChangeHandler.changeDetected then return end -- Change alredy detected
         LOG:Message(CLM.L["Guild change detected. Disabling synchronisation until UI reload."])
         CLM.MODULES.LedgerManager:Cutoff()
         CLM.MODULES.LedgerManager:DisableAdvertising()
         CLM.MODULES.Database:ForceFallback()
         CLM.MODULES.GuildInfoListener:WipeAll()
         CLM.MODULES.Minimap:UpdateIcon()
+        GlobalGuildChangeHandler.changeDetected = true
     end)
 end
 CLM.GlobalGuildChangeHandler = GlobalGuildChangeHandler
