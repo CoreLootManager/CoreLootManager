b0bc7f21ed2d600bb4dc3dcdf57769205c636499
lantisnt <lantisnt@gmail.com>
2024-03-03 15:33:15 +0100

    Add actual guild check

--- ClassicLootManager/Modules/Global/GlobalGuildChangeHandler.lua
+++ ClassicLootManager/Modules/Global/GlobalGuildChangeHandler.lua
@@ -10,8 +10,10 @@ local GlobalGuildChangeHandler = {
     changeDetected = false
 }
 function GlobalGuildChangeHandler:Initialize()
+    self.isInGuild = IsInGuild()
     CLM.MODULES.EventManager:RegisterWoWEvent({"PLAYER_GUILD_UPDATE"}, function(_, _, unitTarget)
         if unitTarget ~= "player" then return end
+        if self.isInGuild == IsInGuild() then return end -- no change
         if GlobalGuildChangeHandler.changeDetected then return end -- Change alredy detected
         LOG:Message(CLM.L["Guild change detected. Disabling synchronisation until UI reload."])
         CLM.MODULES.LedgerManager:Cutoff()
@@ -20,6 +22,7 @@ function GlobalGuildChangeHandler:Initialize()
         CLM.MODULES.GuildInfoListener:WipeAll()
         CLM.MODULES.Minimap:UpdateIcon()
         GlobalGuildChangeHandler.changeDetected = true
+        self.isInGuild = IsInGuild()
     end)
 end
 CLM.GlobalGuildChangeHandler = GlobalGuildChangeHandler
