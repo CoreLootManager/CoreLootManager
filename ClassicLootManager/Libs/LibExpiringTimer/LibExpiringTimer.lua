bf7f1697e8b0df2c66aba8a94342637b60a2c1a1
lantisnt <lantisnt@gmail.com>
2024-03-09 12:18:55 +0100

    Throttle comms when sending on RAID

--- ClassicLootManager/Libs/LibExpiringTimer/LibExpiringTimer.lua
+++ ClassicLootManager/Libs/LibExpiringTimer/LibExpiringTimer.lua
@@ -6,27 +6,20 @@ if not LibExpiringTimer then return end
 
 
 ExpiringTimer = {
-    __call = function(self) return self.Refresh(self) end
+    __call = function(self) return self.Refresh(self) end,
 }
 
 local function spawn(self)
-    if not self.__expiration_callback then
-        self.__expiration_callback = function()
-            self.__expired = true
-            if not self.__ticker:IsCancelled() then
-                self.__ticker:Cancel()
-            end
-            self.callback()
-        end
-    end
-
-    self.__expired = false
-    self.__ticker = C_Timer.NewTicker(self.time, self.__expiration_callback)
+    self.__ticker = C_Timer.NewTimer(self.time, function()
+        self.__ticker = nil
+        self.callback()
+    end)
 end
 
 function ExpiringTimer:Refresh()
-    if not self.__expired and self.__ticker then
+    if self.__ticker then
         self.__ticker:Cancel()
+        self.__ticker = nil
     end
 
     spawn(self)
@@ -34,23 +27,25 @@ function ExpiringTimer:Refresh()
     return self
 end
 
-local function NewTimer(time, callback)
+local function NewExpiringTimer(time, callback)
     if type(time) ~= "number" then
         error(("Argument #1 to `NewTimer' must be a number, got %s."):format(type(time)), 2)
     end
     if type(callback) ~= "function" then
-        error(("Argument #1 to `NewTimer' must be a function, got %s."):format(type(callback)), 2)
+        error(("Argument #2 to `NewTimer' must be a function, got %s."):format(type(callback)), 2)
     end
-
     local o = {}
-
     setmetatable(o, ExpiringTimer)
     ExpiringTimer.__index = ExpiringTimer
-
     o.time = time
     o.callback = callback
-
     return o
 end
 
-setmetatable(LibExpiringTimer, { __call = function(_, ...) return NewTimer(...) end })
+LibExpiringTimer.New = NewExpiringTimer
+
+setmetatable(LibExpiringTimer, {
+    __call = function(_, ...)
+        return NewExpiringTimer(...)
+    end
+})
